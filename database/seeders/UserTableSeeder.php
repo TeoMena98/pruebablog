0a6eff249e164e827b8f16d4c02ea4b513a45862
Mateo Ramírez Mena <mateomena10@gmail.com>
2021-04-01 17:27:36 -0500

    first commit

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'rakibul373@gmail.com',
+            'password' => bcrypt('3980318'),
+            'is_admin' => 1
+        ]);
+    }
+}
